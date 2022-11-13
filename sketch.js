945d9363a92dc8b4336ab99e8300f637110afba3
Lamperouge-l2 <118115321+Lamperouge-l2@users.noreply.github.com>
2022-11-13 03:34:40 -1100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,67 +1,86 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+//name spacing
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+var myengine,myworld;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(800,400);
+   
+    myengine = Engine.create();
+    myworld = myengine.world;
+
+    //Code for brown ground
+    var ground_options ={
+        isStatic: true, 
+    }
+
+    ground = Bodies.rectangle(400,390,800,20,ground_options);
+    World.add(myworld,ground);
+    console.log(ground);
+
+    // Code for red box
+    var redBox_options ={
+        isStatic: true,
+    }
+
+    redBox = Bodies.rectangle(270,320,100,120,redBox_options);
+    World.add(myworld,redBox);
+
+    // Code for green box
+    var greenBox_options ={
+        isStatic: true,
+    }
+
+    greenBox = Bodies.rectangle(390,320,100,120,greenBox_options);
+    World.add(myworld,greenBox);
+
+    // Code for yellow box
+    var yellowBox_options ={
+        isStatic: true,
+        angle:-60
+        
+    }
+
+    yellowBox = Bodies.rectangle(590,320,100,20,yellowBox_options);
+    // Matter.Body.setAngle(yellowBox,130)
+    World.add(myworld,yellowBox);
+
+    // Code for ball
+    var ball_options ={
+        isStatic : false,
+        restitution: 1.6
+    }
+
+    ball = Bodies.circle(590,100,20,ball_options);
+    World.add(myworld,ball);
+
 }
 
-function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
+function draw(){
+    background("lightgreen");
+    Engine.update(myengine);
+    rectMode(CENTER);
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
- 
-}
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	Matter.Body.setStatic(packageBody,false);
-
+    fill("brown")
+    rect(ground.position.x,ground.position.y,800,20);
+    fill("red")
+    rect(redBox.position.x,redBox.position.y,100,120);
+    fill("green")
+    rect(greenBox.position.x,greenBox.position.y,100,120);
     
-  }
+    fill("yellow")
+    push();
+    translate(yellowBox.position.x,yellowBox.position.y);
+    rotate(-45);
+    rectMode(CENTER);
+    rect(0, 0, 100, 20);
+    pop();
+   
+    fill("red")
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
-
-
-
